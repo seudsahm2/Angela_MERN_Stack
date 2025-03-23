9689d2f32b406dbf7b867aca169d9eb5f4aabd81
seud <seudsahm1@gmail.com>
2025-03-23 14:47:01 +0300

    many things done

---
+++ Backend/3.5 Secrets Project/index.js
@@ -0,0 +1,38 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+var userIsAuthorized  = false;
+app.use(bodyParser.urlencoded({extended:true}));
+
+function PasswordCheck(req,res,next){
+    if(req.body["password"] === "ILoveProgramming"){
+        userIsAuthorized = true;
+    }
+    next();
+}
+
+app.use(PasswordCheck);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.post("/check",(req,res)=>{
+    if(userIsAuthorized){
+        res.sendFile(__dirname + "/public/secret.html")
+    }else{
+        res.sendFile(__dirname + "/public/index.html")
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`the server is running on port ${port} \nto close the server click CTRL + C`)
+})
